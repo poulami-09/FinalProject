85d3f87e0bb3cd320e35bc5b7981bd4485ad1eb8
poulami-09 <96105992+poulami-09@users.noreply.github.com>
2023-03-02 15:17:02 +0530

    Update movement.py

--- movement.py
+++ movement.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO          
 from time import sleep
 
-in1 = 24
-in2 = 23
-in3 = 22
-in4 = 21
+in1 = 24 #left1 
+in2 = 23 #right 1
+in3 = 22 #left 2
+in4 = 21 #right 2
 
 TRIG=20
 ECHO=19
@@ -24,14 +24,17 @@ GPIO.output(in2,GPIO.LOW)
 GPIO.output(in3,GPIO.LOW)
 GPIO.output(in4,GPIO.LOW)
 
-    print("waiting for sensor to settle")
-    time.sleep(0.2)
-    GPIO.output(TRIG,True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG,False)
-    while GPIO.input(ECHO)==0:
-        pulse_start=time.time()
-    while GPIO.input(ECHO)==1:
+GPIO.setup(11,GPIO.OUT) #Servo motor
+
+#Ultrasonic sensor
+print("waiting for sensor to settle")
+time.sleep(0.2)
+GPIO.output(TRIG,True)
+time.sleep(0.00001)
+GPIO.output(TRIG,False)
+     while GPIO.input(ECHO)==0:
+       pulse_start=time.time()
+     while GPIO.input(ECHO)==1:
         pulse_end=time.time()
     pulse_duration=pulse_end-pulse_start
     distance=pulse_duration*17150
@@ -51,22 +54,33 @@ def wheels(char x):
          
          if(x=='f'):
          GPIO.output(in1,GPIO.HIGH)
-         GPIO.output(in2,GPIO.LOW)
+         GPIO.output(in2,GPIO.HIGH)
+         GPIO.output(in3,GPIO.LOW)
+         GPIO.output(in4,GPIO.LOW)
          print("forward")
 
         elif (x=='b'):
          GPIO.output(in1,GPIO.LOW)
-         GPIO.output(in2,GPIO.HIGH)
+         GPIO.output(in2,GPIO.LOW)
+         GPIO.output(in3,GPIO.HIGH)
+         GPIO.output(in4,GPIO.HIGH)
          print("backward")
-         x='z'
-
 
         elif x=='s':
          print("stop")
          GPIO.output(in1,GPIO.LOW)
          GPIO.output(in2,GPIO.LOW)
+         GPIO.output(in3,GPIO.LOW)
+         GPIO.output(in41,GPIO.LOW)
 
 
         else:
          print("<<<  wrong data  >>>")
          print("please enter the defined data to continue.....")
+ def rotate():
+    servo.start(0)
+    print ("Rotating 45 degrees")
+   duty = 2
+   while duty <= 14:
+     servo.ChangeDutyCycle(duty)
+     duty = duty + 1
