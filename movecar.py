906765be1c5f0ce0d03d7a5f49f9c0eda537c78e
poulami-09 <96105992+poulami-09@users.noreply.github.com>
2023-03-23 23:57:14 +0530

    Create movecar.py

---
+++ movecar.py
@@ -0,0 +1,142 @@
+import RPi.GPIO as GPIO
+import time
+from gpiozero import servo
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+//MOTOR A
+ENA=25
+IN1=19
+IN2=16
+
+GPIO.setup(ENA,GPIO.OUT)
+ena_pwm=GPIO.PWM(ENA,100)
+GPIO.setup(IN1,GPIO.OUT)
+GPIO.setup(IN2,GPIO.OUT)
+
+//MOTOR B
+
+ENB=23
+IN3=26
+IN4=20
+
+GPIO.setup(ENB,GPIO.OUT)
+enb_pwm=GPIO.PWM(EBA,100)
+GPIO.setup(IN3,GPIO.OUT)
+GPIO.setup(IN4,GPIO.OUT)
+//servo motor
+GRIO.setup(SERVO_TRIG,GPIO.OUT)
+servo=Servo(SERVO_TRIG)
+
+//ULTRASONIC SENSOR
+ECHO=7
+TRIG=22
+GPIO.setup(ECHO,GPIO.IN)
+GPIO.setup(TRIG,GPIO.OUT)
+
+//CAR MOVEMENTS
+def forward():
+    ena_pwm.start(100)
+    enb_pwm.start(100)
+    GPIO.output(IN1,GPIO.HIGH)
+    GPIO.output(IN2,GPIO.LOW)
+    GPIO.output(IN3,GPIO.HIGH)
+    GPIO.output(IN4,GPIO.LOW)
+
+def reverse():
+    ena_pwm.start(100)
+    enb_pwm.start(100)
+    GPIO.output(IN1,GPIO.LOW)
+    GPIO.output(IN2,GPIO.HIGH)
+    GPIO.output(IN3,GPIO.LOW)
+    GPIO.output(IN4,GPIO.HIGH)
+
+def left():
+    ena_pwm.start(100)
+    enb_pwm.start(100)
+    GPIO.output(IN1,GPIO.HIGH)
+    GPIO.output(IN2,GPIO.LOW)
+    GPIO.output(IN3,GPIO.LOW)
+    GPIO.output(IN4,GPIO.HIGH)
+
+def right():
+    ena_pwm.start(100)
+    enb_pwm.start(100)
+    GPIO.output(IN1,GPIO.LOW)
+    GPIO.output(IN2,GPIO.HIGH)
+    GPIO.output(IN3,GPIO.HIGH)
+    GPIO.output(IN4,GPIO.LOW)
+
+def stop():
+    ena_pwm.start(0)
+    enb_pwm.start(0)
+    GPIO.output(IN1,GPIO.LOW)
+    GPIO.output(IN2,GPIO.LOW)
+    GPIO.output(IN3,GPIO.LOW)
+    GPIO.output(IN4,GPIO.LOW)
+  //  time.sleep(tme)
+  
+def uturn():
+    ena_pwm.start(0)
+    enb_pwm.start(0)
+    GPIO.output(IN1,GPIO.HIGH)
+    GPIO.output(IN2,GPIO.LOW)
+    GPIO.output(IN3,GPIO.LOW)
+    GPIO.output(IN4,GPIO.HIGH)
+    time.sleep(tme)
+
+
+//measuring distance from ultrasonic sensor
+def measure():
+  GPIO.output(TRIG,1)
+  time.sleep(0.0001)
+  GPIO.output(TRIG,0)
+  
+  while GPIO.input(ECHO)==0:
+    pass
+  start=time.time()
+  while GPIO.input(ECHO)==1:
+    pass
+  stop=time.time()
+  d=(stop-start)*1700
+
+ //for continuous checking of distance
+sc=0
+sr=0
+sl=0
+dist=0
+try:
+  while True:
+    
+    servo.max() //left
+    time.sleep(0.5)
+    sl=0
+    
+    dist=measure()
+    if dist<=20.0:
+      sl=1
+      
+    servo.max() //middle
+    time.sleep(0.5)
+    sc=0
+    
+    dist=measure()
+    if dist<=20.0:
+      sc=1
+    
+    servo.max() //right
+    time.sleep(0.5)
+    sr=0
+    
+    dist=measure()
+    if dist<=20.0:
+      sl=1
+      
+   join=str(sl)+str(sc)+str(sr)
+  print(join)
+  
+  //8 combinations are possible
+  if join=="000" or join=="101": //Go forward
+    forward()
+    print("Straight")
+    
